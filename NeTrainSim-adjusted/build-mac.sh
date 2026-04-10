f5fa345be39502dfc94ae9ba628cab026a3d25c4
Aziz Kayumov <kayumovabduaziz@gmail.com>
2026-04-10 10:45:42 +0500

    Add command to create 'res' directory
    Create 'res' directory before running NeTrainSimConsole.

--- NeTrainSim-adjusted/build-mac.sh
+++ NeTrainSim-adjusted/build-mac.sh
@@ -1,5 +1,6 @@
 cmake -B build-mac -DCMAKE_BUILD_TYPE=Release -DBUILD_GUI=OFF -DBUILD_SERVER=OFF -DCMAKE_PREFIX_PATH=$(brew --prefix qt6)
 cmake --build build-mac --target NeTrainSimConsole -j$(sysctl -n hw.logicalcpu)
+mkdir -p res
 ./build-mac/src/NeTrainSimConsole/NeTrainSim \
   -n src/data/sampleProject/nodesFile.dat \
   -l src/data/sampleProject/linksFile.dat \
